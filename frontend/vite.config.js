172e59002111400211003fb3bf7cbc8f19399d07
Jayshu <madhursingh2704@gmail.com>
2026-04-03 00:51:03 +0530

    Bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,19 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        // target: "http://localhost:8000", // your backend port
-        target: env.VITE_BACKEND_URL,
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
